26a7e2d053f03c418502688f64a2a28f66ea85d6
Amen Wondwosen <amenwond408@gmail.com>
2023-04-15 18:22:20 -0700

    reduced default page limit to 3

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 class APIHandler:
     def __init__(self) -> None:
-        self.DEFAULT_PAGE_LIMIT = 5     # Max number of pages to look at
+        self.DEFAULT_PAGE_LIMIT = 3     # Max number of pages to look at
 
     def get_all(self, start_page, media_type, all_: bool = False, retry_once: bool = True):
         pass
