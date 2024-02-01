6eff978adb0e9f9b7915f5fd06dfd974657afe3e
Amen Wondwosen <amenwond408@gmail.com>
2024-02-01 10:47:40 -0800

    Refactor base api handler class to an abstract class

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,10 @@
-class APIHandler:
+from abc import ABC, abstractclassmethod
+
+
+class APIHandler(ABC):
     def __init__(self) -> None:
         self.DEFAULT_PAGE_LIMIT = 3     # Max number of pages to look at
 
+    @abstractclassmethod
     def get_all(self, start_page, media_type, all_: bool = False, retry_once: bool = True):
         pass
